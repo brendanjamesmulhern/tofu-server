ca774a691277b75267f77b3e951d9c70f0b8e8ea
earth-r <88467838+earth-r@users.noreply.github.com>
2021-08-16 01:23:17 -0400

    deploy

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/createStripeUrl', async function(req, res) {
 });
 
 app.post('/createPaymentIntent', async function(req, res) {
-	const paymentIntent = await stripe.paymentIntents.create({
+	const intent = await stripe.paymentIntents.create({
 		payment_method_types: ['card'],
 		amount: req.body.price * 100,
 		currency: 'usd',
